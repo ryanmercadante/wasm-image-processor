c90d1cfb2070b9746c0eabf7b9213c852a9ff00f
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2021-11-11 23:19:13 -0500

    add starter files, setup webpack

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './public/main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+}
