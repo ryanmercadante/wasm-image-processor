877ae7bc96bf57dbabdf4003061cda85d81d7cd5
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2021-11-11 23:44:16 -0500

    setup webpack to compile rust to web assembly

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const WasmPackPlugin = require('@wasm-tool/wasm-pack-plugin')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
@@ -10,6 +11,9 @@ module.exports = {
   plugins: [
     new HTMLWebpackPlugin({
       template: './public/index.html',
+    }),
+    new WasmPackPlugin({
+      crateDirectory: path.resolve(__dirname, '.'),
     }),
   ],
 }
