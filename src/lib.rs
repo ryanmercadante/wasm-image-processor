b5d4a5a17d0231b0f89c71696c458d3d07e583d8
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2021-11-12 00:23:44 -0500

    add base64 crate to decode file

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,13 @@
 extern crate wasm_bindgen;
 
+use base64::decode;
 use wasm_bindgen::prelude::wasm_bindgen;
 use web_sys::console::log_1 as log;
 
 #[wasm_bindgen]
 pub fn grayscale(encoded_file: &str) {
-    log(&encoded_file.into());
+    log(&"Grayscale called".into());
+
+    let base64_to_vector = decode(encoded_file).unwrap();
+    log(&"Image decoded".into());
 }
