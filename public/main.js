2b3f536eba8001c18ce030ad30b65510316798ba
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2021-11-11 23:27:35 -0500

    setup javascript to read file

--- public/main.js
+++ public/main.js
@@ -1,1 +1,19 @@
-console.log('test');
+function init() {
+  const input = document.getElementById('upload')
+  const fileReader = new FileReader()
+
+  fileReader.onloadend = () => {
+    const base64 = fileReader.result.replace(
+      /^data:image\/(png|jpeg|jpg);base64,/,
+      ''
+    )
+    console.log(input.files[0])
+    console.log(base64)
+  }
+
+  input.addEventListener('change', () => {
+    fileReader.readAsDataURL(input.files[0])
+  })
+}
+
+init()
