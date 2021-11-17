e60b13fa0b26c8e74d1bef8900440645dd75e042
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2021-11-16 22:10:38 -0500

    grayscale image, write to buffer, encode to base64, return

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,30 @@
 extern crate wasm_bindgen;
 
-use base64::decode;
+use base64::{decode, encode};
 use image::load_from_memory;
+use image::ImageOutputFormat::Png;
 use wasm_bindgen::prelude::wasm_bindgen;
 use web_sys::console::log_1 as log;
 
 #[wasm_bindgen]
-pub fn grayscale(encoded_file: &str) {
+pub fn grayscale(encoded_file: &str) -> String {
     log(&"Grayscale called".into());
 
     let base64_to_vector = decode(encoded_file).unwrap();
     log(&"Image decoded".into());
 
-    let _img = load_from_memory(&base64_to_vector).unwrap();
+    let mut img = load_from_memory(&base64_to_vector).unwrap();
     log(&"Image loaded".into());
+
+    img = img.grayscale();
+    log(&"Grayscale effect applied".into());
+
+    let mut buffer = vec![];
+    img.write_to(&mut buffer, Png).unwrap();
+    log(&"New image written".into());
+
+    let encoded_img = encode(&buffer);
+    let data_url = format!("data:image/png;base64,{}", encoded_img);
+
+    data_url
 }
