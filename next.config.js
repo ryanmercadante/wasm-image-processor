a6657c034e4df74904bea38c7cef054c400b4aac
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2021-11-17 16:16:40 -0500

    add prebuild script, only run wasm pack during development

--- next.config.js
+++ next.config.js
@@ -1,17 +1,21 @@
 const path = require('path')
 const WasmPackPlugin = require('@wasm-tool/wasm-pack-plugin')
 
+const isDevelopment = process.env.NODE_ENV !== 'production'
+
 /**
  * @type {import('next').NextConfig}
  */
 module.exports = {
   webpack: (config, options) => {
-    config.plugins = [
-      ...config.plugins,
-      new WasmPackPlugin({
-        crateDirectory: path.resolve(__dirname, '.'),
-      }),
-    ]
+    if (isDevelopment) {
+      config.plugins = [
+        ...config.plugins,
+        new WasmPackPlugin({
+          crateDirectory: path.resolve(__dirname, '.'),
+        }),
+      ]
+    }
 
     config.experiments = {
       ...config.experiments,
--- scripts/install-wasm-pack.sh
+++
@@ -1,1 +0,0 @@
-curl https://alexcrichton.github.io/wasm-pack/installer/init.sh -sSf | sh
